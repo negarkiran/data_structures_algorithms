e8b257e25876a7d75f2054a8cc6b2d88b036ee41
Kiran Hegde <khegde@equalexperts.com>
2020-07-06 20:33:08 +0530

    datastructure -> stack

---
+++ src/main/java/datastructures/Stack.java
@@ -0,0 +1,38 @@
+package datastructures;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Stack {
+
+    private Integer[] stack;
+    private Integer top = -1;
+    private Integer size;
+
+    public Stack(Integer size) {
+        this.size = size;
+        this.stack = new Integer[size];
+    }
+
+    public Integer push(Integer number) {
+        long stackContent = Arrays.stream(stack).filter(Objects::nonNull).count();
+        if (stackContent < size) {
+            stack[++top] = number;
+            return number;
+        }
+        throw new RuntimeException("Stack is full");
+    }
+
+    public Integer pop() {
+        if (top < 0) {
+            throw new RuntimeException("Stack is empty");
+        }
+        Integer topItem = stack[top];
+        stack[top--] = null;
+        return topItem;
+    }
+
+    public Integer[] get() {
+        return stack;
+    }
+}
---
+++ src/test/java/datastructures/StackTest.java
@@ -0,0 +1,55 @@
+package datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
+public class StackTest {
+
+    private Stack stack;
+
+    @Before
+    public void setup() {
+        stack = new Stack(5);
+    }
+
+    @Test
+    public void shouldPushItem() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        assertEquals(4, Arrays.stream(stack.get()).filter(Objects::nonNull).count());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWhenStackIsFull() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+    }
+
+    @Test
+    public void shouldPopItem() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        assertEquals(4, stack.pop(), 0);
+        assertEquals(3, stack.pop(), 0);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWhenStackGoesEmptyWhilePopping() {
+        stack.push(1);
+        stack.pop();
+        stack.pop();
+    }
+}
