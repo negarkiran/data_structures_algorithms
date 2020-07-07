63dc652b253e7b8b45cd0d07a61d065572b4aab9
Kiran Hegde <khegde@equalexperts.com>
2020-07-07 22:34:59 +0530

    datastructure queue

---
+++ src/main/java/datastructures/Queue.java
@@ -0,0 +1,41 @@
+package datastructures;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Queue {
+
+    private Integer[] queue;
+    private int left;
+    private int right;
+    private int size;
+
+    public Queue(int size) {
+        this.size = size;
+        this.right = 0;
+        this.left = 0;
+        this.queue = new Integer[size];
+    }
+
+    public boolean add(Integer number) {
+        long itemsLength = Arrays.stream(queue).filter(Objects::nonNull).count();
+        if (itemsLength < size) {
+            queue[right++] = number;
+            return true;
+        }
+        throw new RuntimeException("Queue Is Full");
+    }
+
+    public Integer[] get() {
+        return queue;
+    }
+
+    public int remove() {
+        if (left == right) {
+            throw new RuntimeException("Queue is Empty");
+        }
+        int item = queue[left];
+        queue[left++] = null;
+        return item;
+    }
+}
---
+++ src/test/java/datastructures/QueueTest.java
@@ -0,0 +1,61 @@
+package datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class QueueTest {
+
+    private Queue queue;
+
+    @Before
+    public void setup() {
+        queue = new Queue(5);
+    }
+
+    @Test
+    public void shouldAddItem() {
+        assertTrue(queue.add(1));
+        assertTrue(queue.add(2));
+        assertEquals(2, Arrays.stream(queue.get()).filter(Objects::nonNull).count());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWhenQueueIsFull() {
+        assertTrue(queue.add(1));
+        assertTrue(queue.add(2));
+        assertTrue(queue.add(3));
+        assertTrue(queue.add(4));
+        assertTrue(queue.add(5));
+        assertTrue(queue.add(6));
+    }
+
+    @Test
+    public void shouldRemoveItemFromRearEnd() {
+        assertTrue(queue.add(1));
+        assertTrue(queue.add(2));
+        assertTrue(queue.add(3));
+
+        assertEquals(1, queue.remove());
+        assertEquals(2, queue.remove());
+        assertEquals(3, queue.remove());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWhenQueueIsEmptyAndTriedToRemoveItem() {
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+    }
+
+}
