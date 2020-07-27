f6f67ac51be19531cd2663cb129096da2af8c965
Kiran Hegde <khegde@equalexperts.com>
2020-07-27 22:02:13 +0530

    insertion sort

---
+++ src/test/java/algorithms/sort/InsertionSortTest.java
@@ -0,0 +1,24 @@
+package algorithms.sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InsertionSortTest {
+
+    private InsertionSort insertionSort;
+
+    @Before
+    public void setup() {
+        insertionSort = new InsertionSort();
+    }
+
+    @Test
+    public void shouldSortArrayUsingBubbleSort() {
+        int[] input = {98,71,99,22,15,17,31};
+        insertionSort.sort(input);
+        assertEquals(99, input[6]);
+    }
+
+}
